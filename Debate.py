a3a6839e4c4421059dc7ee222a06b654e5e7ba35
abellows50 <134068179+abellows50@users.noreply.github.com>
2023-12-18 17:33:04 -0500

    Update Debate.py

--- Debate.py
+++ Debate.py
@@ -1,4 +1,19 @@
+import Flow
 class Debate:
   def __init__(self):
     self.contentions = []
+    self.pointer = [0,0,1]
+  
+  def newContention(self, name):
+    self.contentions.append(Flow.contention(len(self.contentions)),name)
+  
+  def parse(self, cmd:str):
+    if cmd == "exit":
+      return False
+    elif "new" in cmd:
+      thing = cmd.split(" ")
+      if thing[1]=="contention" or thing[1] == "c":
+        newContention(thing[2])
+        
+
     
