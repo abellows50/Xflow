ca05b61561b8eab8d0b780077530af4853d30bf4
Abel Bellows <abellows50@stuy.edu>
2023-12-18 21:03:12 -0500

    added better clarity to the code

--- Xflow.py
+++ Xflow.py
@@ -5,9 +5,18 @@ from colorama import Fore, Back, Style
 ##This should be a program that handles the flow of the debate. 
 ##It should be able to handle the flow of the debate, and the flow of the arguments.
 
+print("Welcome to the Debate Flow Manager.")
+print("Available Commands:")
+print("- To create a new contention: new contention <name>")
+print("- To create a new point: new point <text>")
+print("- To create a new warrant: new warrant <text>")
+print("- To create a new impact: new impact <text>")
+print("- To switch pointers: switch <contention_id>.<point_id>")
+print("- To exit: exit")
+
 debate = Debate.Debate()
 cmd = input(">> ")
 while debate.parse(cmd):
-    cmd = input(Fore.WHITE+">> ")
-    
+    cmd = input(Fore.WHITE + ">> ")
+
     
